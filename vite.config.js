67fad2e8770bddf2a7ce1d536d3ebcb52a3783b6
arkvdaogmail <216140165+arkvdaogmail@users.noreply.github.com>
2025-08-08 19:49:47 -0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,12 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { resolve } from 'path'
 
 export default defineConfig({
-  plugins: [react()],
   build: {
-    outDir: 'dist',
-    emptyOutDir: true,
     rollupOptions: {
-      input: 'index.html'
+      input: {
+        main: resolve(__dirname, 'public/index.html')
+      }
     }
   }
 })
